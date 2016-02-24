4005963dceb9d0eb531d3ba26bb45ca0102ffd49
Janet Ghazizadeh <janetghazizadeh@Janets-MacBook-Pro.local>
2016-02-23 16:44:46 -0800

    Added estimated receive time feature and js stuff

--- server.py
+++ server.py
@@ -7,7 +7,9 @@ from model import connect_to_db, db, Country, Rate, RicePrice, CountryCode
 
 import math
 
+from datetime import datetime, timedelta
 from pytz import country_timezones
+from delorean import Delorean
 
 
 app = Flask(__name__)
@@ -70,6 +72,9 @@ def best_rate():
     #save user's amount to their browser session for use later
     session['amount'] = amount
 
+    receivers_timezone = (country_timezones(country_code_iso2)[0])
+    # receivers_timezone = (' '.join(country_timezones(session['country_code'])))
+
     #decide which rate to use depending on input amount
     if amount <= 200:
         column_to_use = 'rate_under_200'
@@ -103,6 +108,33 @@ def best_rate():
 
     transaction_speed = result.first().transaction_time
 
+    current_time_in_utc = Delorean()
+
+    if transaction_speed == 'Less than one hour':
+        current_time_in_utc += timedelta(hours=1)
+        estimated_receive_date_time = (current_time_in_utc.shift(receivers_timezone))
+        estimated_receive_date_time = estimated_receive_date_time.format_datetime(locale='en_US')
+    elif transaction_speed == '2 days':
+        current_time_in_utc += timedelta(days=2)
+        estimated_receive_date_time = (current_time_in_utc.shift(receivers_timezone))
+        estimated_receive_date_time = estimated_receive_date_time.format_datetime(locale='en_US')
+    elif transaction_speed == '3-5 days':
+        current_time_in_utc += timedelta(days=5)
+        estimated_receive_date_time = (current_time_in_utc.shift(receivers_timezone))
+        estimated_receive_date_time = estimated_receive_date_time.format_datetime(locale='en_US')
+    elif transaction_speed == 'Same day':
+        current_time_in_utc += timedelta(hours=2)
+        estimated_receive_date_time = (current_time_in_utc.shift(receivers_timezone))
+        estimated_receive_date_time = estimated_receive_date_time.format_datetime(locale='en_US')
+    elif transaction_speed == 'Next day':
+        current_time_in_utc += timedelta(hours=24)
+        estimated_receive_date_time = (current_time_in_utc.shift(receivers_timezone))
+        estimated_receive_date_time = estimated_receive_date_time.format_datetime(locale='en_US')
+    elif transaction_speed == '6 days or more':
+        current_time_in_utc += timedelta(days=6)
+        estimated_receive_date_time = (current_time_in_utc.shift(receivers_timezone))
+        estimated_receive_date_time = estimated_receive_date_time.format_datetime(locale='en_US')
+
     payment_method = result.first().transaction_type
 
     result_country_rice_price = RicePrice.query.filter_by(country_name=country_name)
@@ -119,22 +151,21 @@ def best_rate():
 
         days_fed = int(days_fed)
 
-        second_best_rate = result.offset(1).limit(1).all()
+    second_best_rate = result.offset(1).limit(1).all()
 
-        second_best_comp = second_best_rate[0].company
+    second_best_comp = second_best_rate[0].company
 
-        second_best_fee = second_best_rate[0].rate_under_200
+    second_best_fee = second_best_rate[0].rate_under_200
 
-        second_best_estimate_fees = ((float(second_best_fee) * .01) * amount)
+    second_best_estimate_fees = ((float(second_best_fee) * .01) * amount)
 
-        second_best_total = ((float(second_best_fee) * .01) * amount) + amount
+    second_best_total = ((float(second_best_fee) * .01) * amount) + amount
 
-        second_best_transaction_speed = second_best_rate[0].transaction_time
+    second_best_transaction_speed = second_best_rate[0].transaction_time
 
-        second_best_payment_method = second_best_rate[0].transaction_type
+    second_best_payment_method = second_best_rate[0].transaction_type
 
         # return render_template("other_options.html", second_best_rate=second_best_rate)
-
 
         # render_template("best_rate.html", variable_dict=variables_in_a_dictionary)
 
@@ -144,41 +175,28 @@ def best_rate():
 # }
 
 # <expression1> if <condition> else <expression2>
-
-    
-        return render_template("best_rate.html",
-                               best_company=best_company,
-                               best_rate=best_rate,
-                               estimate_fees=estimate_fees,
-                               total_estimate=total_estimate,
-                               transaction_speed=transaction_speed,
-                               payment_method=payment_method,
-                               second_best_comp=second_best_comp,
-                               second_best_fee=second_best_fee,
-                               second_best_estimate_fees=second_best_estimate_fees,
-                               second_best_total=second_best_total,
-                               second_best_transaction_speed=second_best_transaction_speed,
-                               second_best_payment_method=second_best_payment_method,
-                               amt_of_rice_whole=amt_of_rice_whole,
-                               amt_of_rice=amt_of_rice,
-                               days_fed=days_fed)
+    return render_template("best_rate.html",
+                           best_company=best_company,
+                           best_rate=best_rate,
+                           estimate_fees=estimate_fees,
+                           total_estimate=total_estimate,
+                           transaction_speed=transaction_speed,
+                           estimated_receive_date_time=estimated_receive_date_time,
+                           payment_method=payment_method,
+                           second_best_comp=second_best_comp,
+                           second_best_fee=second_best_fee,
+                           second_best_estimate_fees=second_best_estimate_fees,
+                           second_best_total=second_best_total,
+                           second_best_transaction_speed=second_best_transaction_speed,
+                           second_best_payment_method=second_best_payment_method,
+                           amt_of_rice_whole=amt_of_rice_whole,
+                           amt_of_rice=amt_of_rice,
+                           days_fed=days_fed)
 
 
 # @app.route('/other_options')
 # def show_other_options():
 #     """Details on the other options available"""
-
-
-
-@app.route('/select_rate', methods=['GET'])
-def select_rate():
-    """Details on the selected rate"""
-
-    receivers_timezone = (' '.join(country_timezones(session['country_code'])))
-
-    # estimated_time_in_receivers_tz =
-
-    return render_template("select_rate.html", receivers_timezone=receivers_timezone)
 
 
 if __name__ == "__main__":
